b58b25f1f6e881f6505aead832197f014007a082
Bruce-ym <ychen983@usc.edu>
2020-01-31 16:07:30 -0800

    virtualenv

---
+++ virtualenv/env1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yimingchen/Desktop/20 spring/Growth_Project/virtualenv/env1/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
